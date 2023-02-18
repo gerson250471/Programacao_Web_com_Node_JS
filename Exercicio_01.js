898a2ce20f23b59111103126864053725d015806
Gerson Bernardo da Silva <gbds25@gmail.com>
2023-02-18 14:33:41 -0300

    Exercício 01

---
+++ Exercicio_01.js
@@ -0,0 +1,14 @@
+//Declaração e inicialização de duas variáveis
+let a=5;
+let b=3;
+//Varios exemplos de operações sobre variáveis
+console.log('a=' +a);
+console.log('b=' +b);
+console.log('a+b=' +(a + b));
+console.log('a-b=' +(a - b));
+console.log('a*b=' +(a * b));
+console.log('a/b=' +(a / b));
+console.log('a++=' +(a++));
+console.log('--b=' +(--b));
+console.log('a=' +a);
+console.log('b=' +b);
